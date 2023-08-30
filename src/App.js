b132c0db17489ca055c0c5419c920885ba083fa3
Joseph <joseph.m.troiano@gmail.com>
2023-08-29 20:57:42 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <Router>
-      
         <Header />
-      
     </Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom"
 
+
+
 function Header() {
     return (
         <header>
-            <h1>MuscleMax</h1>
-            <Link to="/">
+            <h1 id="title">MuscleMax</h1>
+            <Link to="/" className="nav-links">
                 Home
+            </Link>
+            <Link to="/workouts" className="nav-links">
+                Workouts
+            </Link>
+            <Link to="/progress" className="nav-links">
+                Progress
+            </Link>
+            <Link to="/settings" className="nav-links">
+                Settings
             </Link>
         </header>
     )
