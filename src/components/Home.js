48707399f8f1a9da8a9fae355b02b6c5c300e53b
Joseph <joseph.m.troiano@gmail.com>
2023-09-04 23:01:21 -0400

    added new functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SignupForm from './SignupForm';
 
 const Home = () => {
+  const [showSignupForm, setShowSignupForm] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false); // Track form submission
+
+  const toggleSignupForm = () => {
+    setShowSignupForm(!showSignupForm);
+
+    // Reset formSubmitted state when cancel is clicked
+    if (formSubmitted) {
+      setFormSubmitted(false);
+    }
+  };
+
+  const handleFormSubmit = () => {
+    // Set formSubmitted state to true when the form is submitted
+    setFormSubmitted(true);
+  };
+
   return (
     <div className="home">
-      <div className="home-content">
-        <h2>Welcome to the MuscleMax App!</h2>
-        <p>Your personal companion for tracking weightlifting workouts and improving your fitness journey.</p>
-        <div className="cta-buttons">
-          <button className="cta-button">Sign up</button>
-          <button className="cta-button">Log in</button>
+      {showSignupForm ? (
+        <div className="signup-form-container">
+          <SignupForm onFormSubmit={handleFormSubmit} />
+          {formSubmitted ? null : (
+            <button className="demo-buttons" onClick={toggleSignupForm}>
+              Cancel
+            </button>
+          )}
         </div>
-      </div>
+      ) : (
+        <div className="home-content">
+          <h2>Welcome to the MuscleMax App!</h2>
+          <p>Your personal companion for tracking weightlifting workouts and improving your fitness journey.</p>
+          <div className="cta-buttons">
+            <button className="demo-buttons" onClick={toggleSignupForm}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,13 +1,25 @@
-import React from "react"
+import React, { useState } from "react";
 
 const Settings = () => {
-    return (
-        <div className="settings">
-            <h2>Account Settings</h2>
-            <h2>Privacy Settings</h2>
-            <h2>Sign Out</h2>
-        </div>
-    )
-}
+  const [showGoodbyeMessage, setShowGoodbyeMessage] = useState(false);
+
+  // Event handler to show the "Goodbye" message
+  const handleSignOutClick = () => {
+    setShowGoodbyeMessage(true);
+
+    // You can perform the sign-out logic here.
+    // For this example, we'll just show the message.
+  };
+
+  return (
+    <div className="settings">
+      {showGoodbyeMessage ? (
+        <h2>Goodbye.</h2>
+      ) : (
+        <h2 onClick={handleSignOutClick}>Sign Out</h2>
+      )}
+    </div>
+  );
+};
 
 export default Settings;
