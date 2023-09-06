919bb43a33df2acf9df5a0b9aa3ac644d3cded5a
Joseph <joseph.m.troiano@gmail.com>
2023-09-05 21:50:57 -0400

    done

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import WorkoutItem from './WorkoutItem';
 
 const WorkoutList = ({ workouts, setWorkouts }) => {
+  const [search, setSearch] = useState('');
+  const [displayedWorkouts, setDisplayedWorkouts] = useState(workouts);
+
+  console.log(search)
   const handleEdit = editedWorkout => {
     const updatedWorkouts = workouts.map(workout =>
       workout.id === editedWorkout.id ? editedWorkout : workout
@@ -9,6 +13,26 @@ const WorkoutList = ({ workouts, setWorkouts }) => {
     setWorkouts(updatedWorkouts);
   };
 
+  const handleInputChange = e => {
+    const searchTerm = e.target.value;
+    setSearch(searchTerm);
+  
+
+    const filteredWorkouts = workouts.filter(workout => {
+      if (searchTerm === "") {
+        return true; 
+      }
+      
+      if (workout.exercise) {
+        return workout.exercise.toLowerCase().includes(searchTerm.toLowerCase());
+      }
+      return false; 
+    });
+  
+    
+    setDisplayedWorkouts(filteredWorkouts);
+  };
+  
   const handleDelete = workoutId => {
     const updatedWorkouts = workouts.filter(workout => workout.id !== workoutId);
     setWorkouts(updatedWorkouts);
@@ -24,7 +48,16 @@ const WorkoutList = ({ workouts, setWorkouts }) => {
   return (
     <div className="workout-list">
       <h2 id="workouts-title">Workouts</h2>
-      {workouts.map(workout => (
+      <label>Search:</label>
+      <input 
+      id="search-workouts"
+      type="text"
+      value={search}
+      placeholder="e.g. Squat"
+      onChange={handleInputChange}
+      />
+      
+      {displayedWorkouts.map(workout => (
         <WorkoutItem
           key={workout.id}
           workout={workout}
@@ -33,7 +66,10 @@ const WorkoutList = ({ workouts, setWorkouts }) => {
         />
       ))}
     </div>
+    
   );
+  
 };
+
 
 export default WorkoutList;
