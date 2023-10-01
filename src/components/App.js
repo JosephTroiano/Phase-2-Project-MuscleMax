d4339f2c02fd2e43da3ea9e37d5d1725775ce291
Joseph <joseph.m.troiano@gmail.com>
2023-09-30 23:01:55 -0400

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 import WorkoutList from "./WorkoutList";
--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import WorkoutItem from "./WorkoutItem";
 
 function WorkoutList({ workouts, onDeleteWorkout, onSave, onToggleFavorite }) {
